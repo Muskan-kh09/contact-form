7b75ffdcb496429231ff349fbb541df58ac50c86
muskan <muskankh9030@gmail.com>
2025-07-07 22:59:30 +0530

    create a new project with contact form

---
+++ src/components/Navigation/Navigation.jsx
@@ -0,0 +1,18 @@
+import styles from "./Navigation.module.css";
+
+const Navigation = () => {
+  return (
+    <nav className={`${styles.navigation} container`}>
+      <div className="logo">
+        <img src="Images/coding-logo.jpg" alt="coding logo" height="100" />
+      </div>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navigation;
